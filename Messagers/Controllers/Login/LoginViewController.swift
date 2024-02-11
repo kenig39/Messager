66f97966e0c31841d0fce4247c614a9c1ba8801c
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-02-11 20:34:42 +0300

    add some views on login

--- Messagers/Controllers/Login/LoginViewController.swift
+++ Messagers/Controllers/Login/LoginViewController.swift
@@ -29,10 +29,43 @@ class LoginViewController: UIViewController {
         field.autocorrectionType = .no
         field.returnKeyType = .continue
         field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
         field.layer.borderColor = UIColor.lightGray.cgColor
-        field.placeholder = "Email Address ...."
+        field.placeholder = "Email Address...."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
         return field
     }()
+    
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .done
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Passoword...."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,17 +76,38 @@ class LoginViewController: UIViewController {
         
         
         //Add subViews
-        view.addSubview(imageView)
+        view.addSubview(scrolView)
+        scrolView.addSubview(imageView)
+        scrolView.addSubview(emailField)
+        scrolView.addSubview(passwordField)
+        scrolView.addSubview(loginButton)
+        
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let size = view.width/3
+        scrolView.frame = view.bounds
         
-        imageView.frame = CGRect(x: (view.width-size)/2,
+        let size = scrolView.width/3
+        imageView.frame = CGRect(x: (scrolView.width-size)/2,
                                  y: 20,
                                  width: size,
                                  height: size)
+        
+        emailField.frame = CGRect(x: 30,
+                                  y: imageView.bottom+10,
+                                  width: scrolView.width-60,
+                                 height: 52)
+        
+      passwordField.frame = CGRect(x: 30,
+                                  y: emailField.bottom+8,
+                                  width: scrolView.width-60,
+                                 height: 52)
+        
+        loginButton.frame = CGRect(x: 30,
+                                  y: passwordField.bottom+10,
+                                  width: scrolView.width-60,
+                                 height: 52)
     }
     
     @objc private func didTapRegister(){
