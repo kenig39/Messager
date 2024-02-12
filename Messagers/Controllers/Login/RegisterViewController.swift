216f388930fb32eb11e890a7285fb17abfc9b3fa
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-02-12 20:41:52 +0300

    add  photoPicker

--- Messagers/Controllers/Login/RegisterViewController.swift
+++ Messagers/Controllers/Login/RegisterViewController.swift
@@ -219,7 +219,7 @@ extension RegisterViewController: UITextFieldDelegate {
     }
 }
 
-extension RegisterViewController: UIImagePickerControllerDelegate {
+extension RegisterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func presentPhotoActionSheet(){
         let actionSheet = UIAlertController(title: "Profile Picture", message: "How would you like to select a picture ", preferredStyle: .actionSheet)
@@ -227,24 +227,46 @@ extension RegisterViewController: UIImagePickerControllerDelegate {
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
         
-        actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { _ in
-            
+        actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { [weak self] _ in
+            self?.presentCamera()
         }))
         
-        actionSheet.addAction(UIAlertAction(title: "Chose Photo", style: .default, handler: { _ in
-            
+        actionSheet.addAction(UIAlertAction(title: "Chose Photo", style: .default, handler: { [weak self] _ in
+             
+            self?.presentPhotoPicker()
+                        
         }))
         
         present(actionSheet, animated: true)
         
     }
     
+    func presentCamera(){
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    func presentPhotoPicker(){
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        
+        picker.dismiss(animated: true)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let selectImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        self.imageView.image = selectImage
         
     }
     
