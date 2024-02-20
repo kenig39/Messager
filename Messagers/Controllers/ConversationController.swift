053d6e00da44e332dbbdf6734f7ce9141b5a85a4
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-02-20 22:37:13 +0300

    add some dependences

--- Messagers/Controllers/ConversationController.swift
+++ Messagers/Controllers/ConversationController.swift
@@ -10,19 +10,46 @@ import FirebaseAuth
 
 class ConversationController: UIViewController {
 
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true
+        return table
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        
-
+        view.addSubview(tableView)
+        view.addSubview(noConversationLabel)
+        setupTableView()
         
     }
+    
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         validateAuth()
     }
+    
+    private let noConversationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "No Conversation!"
+        label.textColor = .gray
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 21, weight: .bold)
+        return label
+    }()
+    
+    
+    private func setupTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    
+    
        // let isloggedIn = UserDefaults.standard.bool(forKey: "logged_in")
         
         private func validateAuth(){
