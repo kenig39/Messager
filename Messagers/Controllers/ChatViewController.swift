2ad654c0373e00c062bbeb698e6069c415f51566
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-02-25 23:56:25 +0300

    add message kit

--- Messagers/Controllers/ChatViewController.swift
+++ Messagers/Controllers/ChatViewController.swift
@@ -9,26 +9,65 @@ import UIKit
 import MessageKit
 
 struct Message: MessageType {
-    
+    var sender: SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKind
 }
+
+
+struct Sender: SenderType {
+    var photoURL: String
+    var senderId: String
+    var displayName: String
+}
+
 
 class ChatViewController: MessagesViewController {
 
+    private var messages = [Message]()
+    
+    private let selfSender = Sender(photoURL: "",
+                                    senderId: "1",
+                                    displayName: "Jon Smith")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        messages.append(Message(sender: selfSender,
+                                messageId: "1",
+                                sentDate: Date(),
+                                kind: .text("hello jone message")))
+        
+        messages.append(Message(sender: selfSender,
+                                messageId: "2",
+                                sentDate: Date(),
+                                kind: .text("hello jone message, just two stroke")))
+
 
         view.backgroundColor = .yellow
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
+
+
+}
+
+extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    
+    func currentSender() -> MessageKit.SenderType {
+        return selfSender
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessageKit.MessagesCollectionView) -> MessageKit.MessageType {
+        return messages[indexPath.section]
     }
-    */
-
+    
+    func numberOfSections(in messagesCollectionView: MessageKit.MessagesCollectionView) -> Int {
+        messages.count
+    }
+    
+    
 }
