f5607e900cdda2d3b0133b7e479b1ee8439b3bd4
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-02-27 23:36:11 +0300

    some code in newConversation

--- Messagers/Controllers/NewConversationViewController.swift
+++ Messagers/Controllers/NewConversationViewController.swift
@@ -6,13 +6,35 @@
 //
 
 import UIKit
+import JGProgressHUD
 
 class NewConversationViewController: UIViewController {
 
+    
+    private let spinner = JGProgressHUD()
+    
     private let searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.placeholder = "Serach for users...."
         return searchBar
+    }()
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+       return table
+    }()
+    
+    private let noresultLable: UILabel = {
+        let lable = UILabel()
+        lable.isHidden = true
+        lable.text = "No Results"
+        lable.textAlignment = .center
+        lable.textColor = .green
+        lable.font = .systemFont(ofSize: 21, weight: .medium)
+        return lable
+        
     }()
     
     override func viewDidLoad() {
@@ -23,6 +45,9 @@ class NewConversationViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel" , style: .done, target: self, action: #selector(dismissSelf))
     }
     
+   @objc private func dismissSelf(){
+        dismiss(animated: true)
+    }
 
 
 }
